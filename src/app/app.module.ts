2911a904ada9e6b38a49258a89ec255d0481b512
nelisiwem <pnpnelly@gmail.com>
2020-09-02 11:46:13 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, MatCardModule, MatGridListModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
