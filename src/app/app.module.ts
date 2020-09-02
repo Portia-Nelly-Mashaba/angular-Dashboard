91615430f781d213d803e045842d6251c11b2a0f
nelisiwem <pnpnelly@gmail.com>
2020-09-02 12:43:52 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
+//import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatCardModule, MatGridListModule, MatButtonModule ],
+  imports:      [ BrowserModule, FormsModule, MatCardModule, MatGridListModule], //MatButtonModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
